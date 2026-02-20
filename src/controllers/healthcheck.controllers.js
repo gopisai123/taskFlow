1a4b1a1613cc0b0eded74c17d4a59bc38f57284a
gopisai123 <gopijanga2006@gmail.com>
2026-02-20 22:28:41 +0530

    healthCheck - api creation

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -1,5 +1,5 @@
 import { ApiResponse } from "../utils/api-response.js";
-// import { asyncHandler } from "../utils/async-handler.js";
+import { asyncHandler } from "../utils/async-handler.js";
 
 /**
 const healthCheck = async (req, res, next) => {
@@ -14,16 +14,8 @@ const healthCheck = async (req, res, next) => {
 };
  */
 
-// const healthCheck = asyncHandler(async (req, res) => {
-//   res.status(200).json(new ApiResponse(200, { message: "Server is running" }));
-// });
-
-const healthCheck = (req, res) => {
-  try {
-    res
-      .status(200)
-      .json(new ApiResponse(200, { message: "Server is running" }));
-  } catch (error) {}
-};
+const healthCheck = asyncHandler(async (req, res) => {
+  res.status(200).json(new ApiResponse(200, { message: "Server is running" }));
+});
 
 export { healthCheck };
